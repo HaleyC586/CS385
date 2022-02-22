7c669dd3e48544ddc8f44e0cd659aa954435c362
HaleyC586 <73767969+HaleyC586@users.noreply.github.com>
2022-02-21 21:52:45 -0800

    Update main.js

--- HW-0/main.js
+++ HW-0/main.js
@@ -1,1 +1,13 @@
+function init() {
+    canvas = document.getElementById("pink");
+    gl = canvas.getContext("webgl2");
 
+    gl.clearColor(241, 221, 207, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+function render() {
+
+}
+
+window.onload = init;
